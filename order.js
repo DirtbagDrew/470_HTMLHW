855c101569c03004b314493b1734db607fb6ffea
Andrew Myer <andrewmyerhb@gmail.com>
2018-03-24 23:34:37 -0700

    b

---
+++ order.js
@@ -0,0 +1,14 @@
+function order()
+{
+  var keepShopping=true;
+
+}
+function newItem()
+{
+  document.write('<select size=3 name = "selectCollection" id="selectCollection1">');
+  document.write('<option>Living Room</option>');
+  document.write('<option>Kitchen</option>');
+  document.write('<option>Bedroom</option>');
+  document.write('<option>Family Room</option>');
+  document.write('</select>');
+}
