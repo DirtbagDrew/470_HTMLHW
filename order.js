cecddf2cca6b4310610ec2f3b80e5ef0b616adbb
Andrew Myer <andrewmyerhb@gmail.com>
2018-03-30 00:41:46 -0700

    figuring out this order form

--- order.js
+++ order.js
@@ -6,9 +6,14 @@ function order()
 function newItem()
 {
   document.write('<select size=3 name = "selectCollection" id="selectCollection1">');
-  document.write('<option>Living Room</option>');
-  document.write('<option>Kitchen</option>');
+  document.write('<option value="1">Living Room</option>');
+  document.write('<option value="1" >Kitchen</option>');
   document.write('<option>Bedroom</option>');
   document.write('<option>Family Room</option>');
   document.write('</select>');
+  var e = document.getElementsById('selectCollection1');
+  if(e=='1')
+  {
+    alert('');
+  }
 }
